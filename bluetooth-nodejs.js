b61e65544a9b8081f9c37fcbca178a775ec9b0db
rubenfsantiago <59794432+rubenfsantiago@users.noreply.github.com>
2021-02-26 21:34:17 +0100

    Update bluetooth-nodejs.js

--- bluetooth-nodejs.js
+++ bluetooth-nodejs.js
@@ -2,7 +2,7 @@ const noble = require('@abandonware/noble');
 
 const ACCELEROMETERSERVICE_SERVICE_UUID = 'e95d0753251d470aa062fa1922dfa9a8';
 const ACCELEROMETERDATA_CHARACTERISTIC_UUID = 'e95dca4b251d470aa062fa1922dfa9a8';
-const ACCELEROMETERPERIOD_CHARACTERISTIC_UUID = 'e95dfb24251d470aa062fa1922dfa9';
+const ACCELEROMETERPERIOD_CHARACTERISTIC_UUID = 'e95dfb24251d470aa062fa1922dfa9a8';
 
 
 noble.on('stateChange', (state) => {
@@ -17,7 +17,7 @@ noble.on('stateChange', (state) => {
 
 noble.on('discover', peripheral => {
   console.log(
-    `Found device, name: ${peripheral.advertisement.localName}, uuid: ${periphe$
+    `Found device, name: ${peripheral.advertisement.localName}, uuid: ${peripheral.uuid}`
   );
 
   if (peripheral.uuid === 'eb9ddad7861f') {
@@ -36,7 +36,7 @@ noble.on('discover', peripheral => {
         console.log(`Found service name: ${services[0].uuid}`); 
         const service =services[0];
 
-        service.discoverCharacteristics(ACCELEROMETERDATA_CHARACTERISTIC_UUID, $
+        service.discoverCharacteristics(ACCELEROMETERDATA_CHARACTERISTIC_UUID, (error, characteristics)=>{
                 const datos=characteristics[0];
                 datos.on('data', (data)=>{
                         datos.read((err,readData) =>{
@@ -52,25 +52,22 @@ noble.on('discover', peripheral => {
                         console.log("SUSCRITO A LA CARACTERISTICA");
                 });
         });
-        //const period = service.discoverCharacteristics(ACCELEROMETERPERIOD_CH$
+        //const period = service.discoverCharacteristics(ACCELEROMETERPERIOD_CHARACTERISTIC_UUID);
 
 
         });
 //      console.log(peripheral.discoverAllServicesAndCharacteristics());
       
-
-
-
 //        service.discoverCharacteristics(null, (error, characteristics) => {
   //        characteristics.forEach(characteristic => {
-    //        console.log(`Found characteristic, name: ${characteristic.name}, $
+    //        console.log(`Found characteristic, name: ${characteristic.name}, uuid: ${characteristic.uuid}, type: ${characteristic.type}, properties: ${characteristic.properties.join(',')}`)
       //    }) //characteristics.foreach
 
           //characteristics.forEach(characteristic => {
-          //  if (characteristic.name === 'System ID' || characteristic.name ==$
-          //    characteristic.read((error, data) => console.log(`${characteris$
+          //  if (characteristic.name === 'System ID' || characteristic.name === 'PnP ID') {
+          //    characteristic.read((error, data) => console.log(`${characteristic.name}: 0x${arrayBufferToHex(data)}`))
           //  } else {
-          //    characteristic.read((error, data) => console.log(`${characteris$
+          //    characteristic.read((error, data) => console.log(`${characteristic.name}: ${data.toString('ascii')}`))
           //  }
          // })//characteristics.forEach
 //        })
@@ -78,6 +75,14 @@ noble.on('discover', peripheral => {
     }) //peripheral.connect
   } //peripheral.uuid
 }) //noble.on discover
+
+
+
+
+
+
+
+
 
 
 
